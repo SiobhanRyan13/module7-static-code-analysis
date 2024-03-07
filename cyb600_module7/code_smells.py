66bd1034f584454a26238c80f90cae6a8417be6f
SiobhanRyan13 <doughers@canisius.edu>
2024-03-06 20:25:04 -0500

    try

--- cyb600_module7/code_smells.py
+++ cyb600_module7/code_smells.py
@@ -1,17 +1,23 @@
-def checkIfPrime(number):
+def check_if_prime(number):
     '''
-    checks if number is prime
+    Check if number is prime
+    checks if number the given number is a prime number
+        number (int):
+    Returns:
+          if temp == 1:
+        return"given number is not prime"
+    else:
+        return"given number is prime"
     '''
-    I, temp = 0, 0
-    n = int(str(int(input("please give a number : "))))
-    for I in range(2, n // 2):
-        if n % I == 0:
+    number = int(str(int(input("please give a number : "))))
+    temp = 0
+    for i in range(2, number // 2):
+        if number % i == 0:
             temp=1
             break
     if temp == 1:
         return"given number is not prime"
-    else:
-        return"given number is prime"
+    return"given number is prime"
 
 if __name__ == "__main__":
-    print(checkIfPrime())
+    print(check_if_prime())
